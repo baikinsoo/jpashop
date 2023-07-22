b092bc403f7357e5e290a9295d3876a40ef25920
baekinsoo <baikinsoo95@gmail.com>
2023-07-22 23:02:26 +0900

    ~ json data에 종속 시키기

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -66,8 +66,7 @@ public class InitDb {
             em.persist(order);
         }
 
-        private Member createMember(String name, String city, String street,
-                                    String zipcode) {
+        private Member createMember(String name, String city, String street, String zipcode) {
             Member member = new Member();
             member.setName(name);
             member.setAddress(new Address(city, street, zipcode));
